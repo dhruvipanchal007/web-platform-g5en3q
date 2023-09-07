5b2077b0ede88f7a49687a97aeeb5d3e74dcbadb
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-07 12:10:59 +0530

    2 files modified

--- answer/2.js
+++ answer/2.js
@@ -12,17 +12,15 @@
 // let numbers = [3, 5, 7, 9, 20];
 // let maxnumber = findmaxvalue(numbers);
 // console.log(maxnumber);
-let num=prompt('enter value');
+let num = prompt('enter value');
 
-function findcorrectnuber(num){
-if(num <0){
- return "negative";
+function findcorrectnuber(num) {
+  if (num < 0) {
+    return 'negative';
+  } else if (num > 0) {
+    return 'positive';
+  } else {
+    return 'zero';
+  }
 }
-else if(num >0){
-  return "positive"
-}
-else{
-  return "zero";
-}
-}
-let ans=findcorrectnuber(num)
+let ans = findcorrectnuber(num);
