b92c84c143a162d1cd612d454f439be9deacf9ee
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-07 11:21:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-g5en3q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-g5en3q)
---
+++ answer/1.js
@@ -0,0 +1,52 @@
+// 1
+// 1,2
+// 1,2,3
+// 1,2,3,4
+
+// for(let i=1;i<5;i++){
+//   let str="";
+//   for(let j=1;j<=i;j++){
+//     str+=j;
+//   }
+//   console.log(str);
+// }
+
+// 1
+// 2,2
+// 3,3,3
+// 4,4,4,4
+// 5,5,5,5
+// for(let i=1;i<6;i++){
+//   let str="";
+//   for(let j=1;j<=i;j++){
+//     str+=i;
+//   }
+//   console.log(str);
+// }
+
+
+// *
+// **
+// ***
+// ****
+// *****
+// for(let i=1;i<6;i++){
+//   let str="";
+//   for(let j=1;j<=i;j++){
+//     str+="*";
+//   }
+//   console.log(str);
+// }
+
+// 1
+// 01
+// 101
+// 0101
+// 10101
+
+for(let i=1;i<6;i++){
+  let str="";
+  for(let j=1; j<=i;j++){
+    if()
+  }
+}
