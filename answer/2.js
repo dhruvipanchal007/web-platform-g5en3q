04ae79002b11ce7011a3a015e02f1ee37b0c45ca
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-07 12:08:35 +0530

    1 file modified

--- answer/2.js
+++ answer/2.js
@@ -1,14 +1,28 @@
-// find  largest value
-function findmaxvalue(numbers) {
-  let largenumber = numbers[0];
-  for (let i = 0; i < numbers.length; i++) {
-    if (largenumber < numbers[i]) {
-      largenumber = numbers[i];
-    }
-  }
-  return largenumber;
-}
+// // find  largest value
+// function findmaxvalue(numbers) {
+//   let largenumber = numbers[0];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (largenumber < numbers[i]) {
+//       largenumber = numbers[i];
+//     }
+//   }
+//   return largenumber;
+// }
 
-let numbers = [3, 5, 7, 9, 20];
-let maxnumber = findmaxvalue(numbers);
-console.log(maxnumber);
+// let numbers = [3, 5, 7, 9, 20];
+// let maxnumber = findmaxvalue(numbers);
+// console.log(maxnumber);
+let num=prompt('enter value');
+
+function findcorrectnuber(num){
+if(num <0){
+ return "negative";
+}
+else if(num >0){
+  return "positive"
+}
+else{
+  return "zero";
+}
+}
+let ans=findcorrectnuber(num)
