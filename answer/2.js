0b400cd08e295e74f9554a1a3b8c4652ba7fe85f
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-07 11:59:43 +0530

    1 file modified

--- answer/2.js
+++ answer/2.js
@@ -2,7 +2,7 @@
 function findmaxvalue(numbers){
   let largenumber=numbers[0];
   for(let i=1;i<numbers.length;i++){
-    if(largenumber>numbers[i]){
+    if(largenumber<numbers[i]){
       largenumber==numbers[i];
     }
   }
