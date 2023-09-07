89d926eeb42b8ab3a358a42b0ed802ffcc2c580e
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-07 11:54:46 +0530

    1 file modified

--- answer/2.js
+++ answer/2.js
@@ -1,14 +1,14 @@
 // find  largest value
-function findmaxvalue(number){
-  let largenumber=number[0];
-  for(let i=0;i<number.length;i++){
-    if(largenumber>number[i]){
-      largenumber===number[i];
+function findmaxvalue(numbers){
+  let largenumber=numbers[0];
+  for(let i=0;i<numbers.length;i++){
+    if(largenumber<numbers[i]){
+      largenumber==numbers[i];
     }
   }
   return largenumber;
 }
 
-let number=[10,20,30,100];
-let maxnumber=findmaxvalue(number);
+let numbers=[10,20,30,100];
+let maxnumber=findmaxvalue(numbers);
 console.log(maxnumber);
