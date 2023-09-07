22d1cf515d4ac35185ec53ea3c09754391167e64
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-07 12:21:41 +0530

    1 file modified

--- answer/2.js
+++ answer/2.js
@@ -12,16 +12,33 @@
 // let numbers = [3, 5, 7, 9, 20];
 // let maxnumber = findmaxvalue(numbers);
 // console.log(maxnumber);
-let num = prompt('enter value');
+// let num = prompt('enter value');
 
-function findcorrectnuber(num) {
-  if (num < 0) {
-    return 'negative';
-  } else if (num > 0) {
-    return 'positive';
-  } else {
-    return 'zero';
+// function findcorrectnuber(num) {
+//   if (num < 0) {
+//     return 'negative';
+//   } else if (num > 0) {
+//     return 'positive';
+//   } else {
+//     return 'zero';
+//   }
+// }
+// let ans = findcorrectnuber(num);
+// console.log(ans);
+
+function findcharacter(c){
+
+  if(c>65 &&c<90){
+    return "capital";
+  }
+  else if(c>97 && c<122){
+    return "small";
+  }
+  else{
+    return "special";
   }
 }
-let ans = findcorrectnuber(num);
+let c=prompt('enter character');
+let ans=findcharacter(c);
 console.log(ans);
+
