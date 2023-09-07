02615feffc1a602584e9bb24c0c3c311c6a81604
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-07 11:43:06 +0530

    1 file modified

--- answer/1.js
+++ answer/1.js
@@ -59,14 +59,15 @@
 // 2,3
 // 4,5,6
 // 7,8,9,10
-for(let i=1;i<5;i++){
-  let str="";
-  for(let j=1;j<=i;j++){
-str+=num;
-    num++;
-    if(j<i){
-    str+="";
-    }
-  }
-  console.log(str);
-}
+// let  num=1;
+// for(let i=1;i<5;i++){
+//   let str="";
+//   for(let j=1;j<=i;j++){
+// str+=num;
+//     num++;
+//     if(j<i){
+//     str+="";
+//     }
+//   }
+//   console.log(str);
+// }
