bb12bd0bf16370ec203ac1e7af53827098797f05
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-08 11:44:35 +0530

    1 file modified

--- answer/2.js
+++ answer/2.js
@@ -115,32 +115,31 @@
 // console.log(ans);
 
 // ***write reverse number
-// let number = prompt('enter number');
-// let num = Number(number);
-// if (isNaN(num)) {
-//   console.log('invalid output');
-// }
-// let reversenumber = 0;
-// let originalnumber = num;
-// while (num > 0) {
-//   let digit = num % 10;
-//   reversenumber = reversenumber * 10 + digit;
-//   num = Math.floor(num / 10);
-// }
-// console.log(reversenumber);
-// console.log(originalnumber);
+let number = 123;
+if (isNaN(num)) {
+  console.log('invalid output');
+}
+let reversenumber = 0;
+let originalnumber = num;
+while (num > 0) {
+  let digit = num % 10;
+  reversenumber = reversenumber * 10 + digit;
+  num = Math.floor(num / 10);
+}
+console.log(reversenumber);
+console.log(originalnumber);
 
 
 // **palindrome number or not
-let number=121;
-if(isNaN(number)){
-  console.log('invalid');
-}
-let reversenumber=0;
-let originalnumber=number;
-while(number>0){
-  let digit=number%10;
-  reversenumber=reversenumber*10+digit;
-  number=Math.floor(number/10);
-}
-console.log(reversenumber);
+// let number=121;
+// if(isNaN(number)){
+//   console.log('invalid');
+// }
+// let reversenumber=0;
+// let originalnumber=number;
+// while(number>0){
+//   let digit=number%10;
+//   reversenumber=reversenumber*10+digit;
+//   number=Math.floor(number/10);
+// }
+// console.log(reversenumber);
