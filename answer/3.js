b60233406ad529bba465cc4d36a8930af82807d9
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-08 11:54:14 +0530

    1 file modified

--- answer/3.js
+++ answer/3.js
@@ -14,22 +14,21 @@
 // console.log(originalnumber);
 
 // **palindrome number or not
-let number=456;
-if(isNaN(number)){
+let number = 121;
+if (isNaN(number)) {
   console.log('invalid');
-}
-let reversenumber=0;
-let originalnumber=number;
-while(number>0){
-  let digit=number%10;
-  reversenumber=reversenumber*10+digit;
-  number=Math.floor(number/10);
+} else {
+  let reversenumber = 0;
+  let originalnumber = number;
+  while (number > 0) {
+    let digit = number % 10;
+    reversenumber = reversenumber * 10 + digit;
+    number = Math.floor(number / 10);
+  }
 
-
-if(reversenumber===number){
-  return "palindromenumber";
-}
-else  {
-  return "palindrome not number";
-}
+  if (reversenumber === originalnumber) {
+    console.log('palindromenumber');
+  } else {
+    console.log('palindrome not number');
+  }
 }
