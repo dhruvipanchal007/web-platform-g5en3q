972ad302f7ff6dec855329712f6e5924c22cd376
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-08 11:43:16 +0530

    1 file modified

--- answer/2.js
+++ answer/2.js
@@ -129,3 +129,18 @@
 // }
 // console.log(reversenumber);
 // console.log(originalnumber);
+
+
+// **palindrome number or not
+let number=121;
+if(isNaN(number)){
+  console.log('invalid');
+}
+let reversenumber=0;
+let originalnumber=number;
+while(number>0){
+  let digit=number%10;
+  reversenumber=reversenumber*10+digit;
+  number=Math.floor(number/10);
+}
+console.log(reversenumber);
