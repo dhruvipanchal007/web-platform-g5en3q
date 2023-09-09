39f9015f8242a2b3f2f745ded76e627b0ab5bcaf
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-09 10:36:07 +0530

    2 files modified, 1 added

--- answer/3.js
+++ answer/3.js
@@ -80,21 +80,3 @@
 // console.log(hn);
 
 // ***armstrongnumber
-function isarmstrong(number){
-let num=number.tostring();
-let  n=num.length;
-let  sum=0;
-for(let i=1;i<n;i++){
-  let digit=Number(num[i]);
-  let sum=Math.pow(digit,n);
-}
-if(sum===number){
-  return true;
-}
-else{
-  return false;
-}
-}
-let number=370;
-let ans=isarmstrong(number);
-console.log(ans);
