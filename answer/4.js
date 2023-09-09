d904c57131c602c1370279fb39cfd551cf268d2c
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-09 10:44:29 +0530

    1 file modified

--- answer/4.js
+++ answer/4.js
@@ -1,4 +1,13 @@
+// let number=[5,3,4,7,10];
+// number.sort(function(a,b){
+//   return a-b;
+// })
+
+// console.log(number);
+
 let number=[5,3,4,7,10];
-sort(function(a,b){
-  
+number.sort(function(a,b){
+  return b-a;
 })
+
+console.log(number);
