a4050d3ed72e1d5cf5c4ee4a6c1343340d20dd7e
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-09 11:48:53 +0530

    1 file modified

--- answer/4.js
+++ answer/4.js
@@ -13,17 +13,17 @@
 // console.log(number);
 
 // ***fibonacci nos number
-let a=0;
-let b=1;
-console.log(a);
-console.log(b);
+// let a=0;
+// let b=1;
+// console.log(a);
+// console.log(b);
 
-for(let i=1;i<25;i++){
-  let c=a+b;
-  console.log(c);
-  a=b;
-  b=c;
-}
+// for(let i=1;i<25;i++){
+//   let c=a+b;
+//   console.log(c);
+//   a=b;
+//   b=c;
+// }
 
 
 
