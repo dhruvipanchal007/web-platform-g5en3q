5e0d3fa70e6c145ce17bfb1ac929df295311720a
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-09 10:28:38 +0530

    1 file modified

--- answer/3.js
+++ answer/3.js
@@ -80,3 +80,15 @@
 // console.log(hn);
 
 // ***armstrongnumber
+function isarmstrong(number){
+let num=number.tostring();
+let  n=num.length;
+let  sum=0;
+for(let i=1;i<n;i++){
+  let digit=Number(num);
+  let sum=Math.pow(digit,n);
+}
+}
+let number=370;
+let ans=isarmstrong(number);
+console.log(ans);
