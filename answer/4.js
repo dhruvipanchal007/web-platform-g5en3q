bfcd6130ee84c5da1240c20ff5943d62672d63a6
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-09 11:11:36 +0530

    1 file modified

--- answer/4.js
+++ answer/4.js
@@ -12,11 +12,17 @@
 
 // console.log(number);
 
-let n = 5;
-let sum = 0;
-for (let i = 0; i <= n; i++) {
-  let number = prompt('enter number');
-  sum += Number(number);
+// let n = 5;
+// let sum = 0;
+// for (let i = 0; i <= n; i++) {
+//   let number = prompt('enter number');
+//   sum += Number(number);
   
-}
-console.log(sum);
+// }
+// console.log(sum);
+
+let number=[10,20,5,3,8];
+number.sort(function(a,b){
+  return a-b;
+})
+console.log(number);
