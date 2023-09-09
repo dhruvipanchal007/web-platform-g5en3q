7b525d655c07207332b4dde9fcbcd00757485863
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-09 11:09:25 +0530

    1 file modified

--- answer/4.js
+++ answer/4.js
@@ -11,3 +11,12 @@
 // })
 
 // console.log(number);
+
+let n = 5;
+let sum = 0;
+for (let i = 0; i <= n; i++) {
+  let number = prompt('enter number');
+  sum += Number(number);
+  
+}
+console.log(sum);
